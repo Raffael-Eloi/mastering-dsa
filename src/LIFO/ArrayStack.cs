c3919eeaefee3859b23879bfd096389773f0a5bb
Raffael Eloi <raffaeleloi121@gmail.com>
2026-02-24 05:31:53 -0300

    add more LIFO examples

---
+++ src/LIFO/ArrayStack.cs
@@ -0,0 +1,33 @@
+﻿namespace LIFO;
+
+public class ArrayStack
+{
+    private int[] items;
+    private int top = -1;
+
+    public ArrayStack(int size)
+    {
+        items = new int[size];
+    }
+
+    public void Push(int value)
+    {
+        if (top == items.Length - 1)
+            throw new Exception("Stack overflow");
+
+        items[++top] = value;
+    }
+
+    public int Pop()
+    {
+        if (top == -1)
+            throw new Exception("Stack underflow");
+
+        return items[top--];
+    }
+
+    public int Peek()
+    {
+        return items[top];
+    }
+}
---
+++ src/LIFO/README.md
@@ -0,0 +1,13 @@
+﻿Last In, First Out
+Like a pile of plates.
+
+Push → add on top
+Pop → remove from top
+
+### Complexity
+
+Push → O(1)
+Pop → O(1)
+Peek → O(1)
+
+No traversal needed.
