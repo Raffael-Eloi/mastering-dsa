0bb5a80ba6e96ebfbdbe6c38a67c2ddb27373ce7
Raffael Eloi <raffaeleloi121@gmail.com>
2026-02-23 22:47:19 -0300

    add LIFO example

---
+++ src/LIFO/MyStack.cs
@@ -0,0 +1,33 @@
+﻿namespace LIFO;
+
+public class MyStack
+{
+    private Node? top;
+
+    public void Push(int value)
+    {
+        var newNode = new Node(value);
+        newNode.Next = top;
+        top = newNode;
+    }
+
+    public int? Pop()
+    {
+        if (top == null)
+            return null;
+
+        int value = top.Value;
+        top = top.Next;
+        return value;
+    }
+
+    public int? Peek()
+    {
+        return top?.Value;
+    }
+
+    public bool IsEmpty()
+    {
+        return top == null;
+    }
+}
---
+++ src/LIFO/Program.cs
@@ -0,0 +1,10 @@
+﻿using LIFO;
+
+var stack = new MyStack();
+
+stack.Push(10);
+stack.Push(20);
+stack.Push(30);
+
+Console.WriteLine(stack.Pop()); // 30
+Console.WriteLine(stack.Pop()); // 20
